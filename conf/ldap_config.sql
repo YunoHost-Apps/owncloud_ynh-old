1a3cd31c582fe09d1eead605614648eb44409641
kload <kload@kload.fr>
2015-05-23 13:25:28 +0200

    [fix] Enable plugins directly from database + minor fixes

--- conf/ldap_config.sql
+++ conf/ldap_config.sql
@@ -1,4 +1,12 @@
 REPLACE INTO oc_appconfig (appid, configkey, configvalue) VALUES
+('files_external', 'enabled', 'yes'),
+('files_external', 'installed_version', '0.2.3'),
+('files_external', 'ocsid', '166048'),
+('files_external', 'types', 'filesystem'),
+('user_ldap', 'enabled', 'yes'),
+('user_ldap', 'installed_version', '0.5.0'),
+('user_ldap', 'ocsid', '166061'),
+('user_ldap', 'types', 'authentication'),
 ('user_ldap', 'ldap_uuid_attribute', 'auto'),
 ('user_ldap', 'ldap_host', 'localhost'),
 ('user_ldap', 'ldap_port', '389'),
@@ -29,3 +37,25 @@ REPLACE INTO oc_appconfig (appid, configkey, configvalue) VALUES
 ('user_ldap', 'ldap_attributes_for_group_search', ''),
 ('user_ldap', 'ldap_expert_username_attr', 'uid'),
 ('user_ldap', 'ldap_expert_uuid_attr', '');
+
+CREATE TABLE IF NOT EXISTS `oc_ldap_group_mapping` (
+  `ldap_dn` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `owncloud_name` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `directory_uuid` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  PRIMARY KEY (`ldap_dn`),
+  UNIQUE KEY `owncloud_name_groups` (`owncloud_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE IF NOT EXISTS `oc_ldap_group_members` (
+  `owncloudname` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `owncloudusers` longtext COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`owncloudname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE IF NOT EXISTS `oc_ldap_user_mapping` (
+  `ldap_dn` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `owncloud_name` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `directory_uuid` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  PRIMARY KEY (`owncloud_name`),
+  UNIQUE KEY `ldap_dn_users` (`ldap_dn`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
