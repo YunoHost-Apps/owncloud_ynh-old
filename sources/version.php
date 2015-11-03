ec217289883aa1d9eb23174cabc000f98b7c2259
zamentur <valentin@grimaud.me>
2015-11-03 03:44:41 +0100

    [enh] Verify sources in install/upgrade script

--- sources/version.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$OC_Version = array(8,2,0,12);
-$OC_VersionString = '8.2.0';
-$OC_Edition = '';
-$OC_Channel = 'stable';
-$OC_VersionCanBeUpgradedFrom = array(8,1);
-$OC_Build = '2015-10-19T13:24:39+00:00 8ee2009de36e01a9866404f07722892f84c16e3e';
